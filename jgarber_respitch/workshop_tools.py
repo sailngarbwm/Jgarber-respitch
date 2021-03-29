f6761260790e8e0581a5dfa4adb83607d71bca5b
JonathanGarber <jonathan.garber@unimelb.edu.au>
2021-03-29 20:20:53 +1100

    added a cheat function
    to get a row of a dataframe in the right dictionary

---
+++ jgarber_respitch/workshop_tools.py
@@ -0,0 +1,21 @@
+
+import pandas as pd 
+def convert_df_row_to_dictionary(df: pd.DataFrame,row_number:int):
+    """
+    converts the row of a dataframe to a dictionary
+    of lists
+
+    :param df: input dataframe
+    :type df: pd.DataFrame
+    :param row_number: row to convert into a dictionary
+    
+    :return: dictionary of the values, where keys are the column names, and the values are lists of the values
+    :rtype: dictionary
+
+    """
+
+    out_dict = ds_mild.loc[row_number].to_dict()
+
+    for key in out_tdict.keys():
+        out_dict[key] = [out_dict[key]]
+    return out_dict
